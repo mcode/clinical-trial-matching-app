0ce9c8030aa952ba207233bdcfe7d55e3ee0f8c3
jtquach1 <jtquach1@gmail.com>
2021-10-14 16:14:58 -0400

    Load necessary FHIR Patient bundle parts into search form

---
+++ src/utils/radiation.ts
@@ -0,0 +1,13 @@
+import { fhirclient } from 'fhirclient/lib/types';
+
+export const convertFhirRadiationProcedures = (bundle: fhirclient.FHIR.Bundle): string[] => {
+  const radiationProcedures = bundle.entry || [];
+  console.log('radiationProcedures', radiationProcedures);
+  const radiations: string[] = [];
+  for (const { resource } of radiationProcedures) {
+    const radiation =
+      resource.code && resource.code.coding && resource.code.coding[0] && resource.code.coding[0].display;
+    radiation ? radiations.push(radiation) : null;
+  }
+  return radiations;
+};
