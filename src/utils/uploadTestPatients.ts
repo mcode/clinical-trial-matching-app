de5d393edbe6ea7749d78c05881df7c876b49e3c
jtquach1 <jtquach1@gmail.com>
2021-10-27 23:04:00 -0400

    More refactoring and fixes - Removed uploading to Moonshot server - Fixed printing of unsuccessful uploads and added diagnostic messages - Updated README with instructions on how to run upload script - Updated sample patient bundles to use different IDs for testing

--- src/utils/uploadTestPatients.ts
+++ src/utils/uploadTestPatients.ts
@@ -1,19 +1,20 @@
 import * as fs from 'fs';
 import * as path from 'path';
 import * as mkdirp from 'mkdirp';
-import { Bundle, BundleEntry, FhirResource } from 'fhir/r4';
+import { Bundle, BundleEntry, FhirResource, OperationOutcome, Patient, Resource } from 'fhir/r4';
 import axios, { AxiosError, AxiosRequestConfig, AxiosResponse } from 'axios';
 
 type Result = {
-  'bundle id': string;
+  'resource id': string;
+  'patient or entries?': 'patient' | 'entries';
   'transaction status': string;
-  'entry statuses': string;
+  'resource statuses': string;
+  'diagnostic message'?: string;
 };
 
-const cloneAndConvertToTransaction = (bundle: Bundle, baseURL: string): Bundle => {
-  const clone: Bundle = JSON.parse(JSON.stringify(bundle));
-  clone.type = 'transaction';
-  const entries: BundleEntry[] = clone.entry;
+const convertToTransaction = (bundle: Bundle, baseURL: string): Bundle => {
+  bundle.type = 'transaction';
+  const entries: BundleEntry[] = bundle.entry;
 
   for (const entry of entries) {
     entry.fullUrl = `${baseURL}/${entry.resource.resourceType}/${entry.resource.id}`;
@@ -23,7 +24,7 @@ const cloneAndConvertToTransaction = (bundle: Bundle, baseURL: string): Bundle =
     };
   }
 
-  return clone;
+  return bundle;
 };
 
 const dumpTestPatientJSON = (release: string, fileName: string, patient: FhirResource, converted: Bundle): void => {
@@ -37,23 +38,46 @@ const dumpTestPatientJSON = (release: string, fileName: string, patient: FhirRes
   fs.writeFileSync(convertedFilepath, JSON.stringify(converted, null, 2), 'utf8');
 };
 
-const getResult = (name: string, response: AxiosResponse<Bundle>): Result => {
-  const status = `${response.status} ${response.statusText}`;
+const getResult = (
+  resourceId: string,
+  resourceName: 'patient' | 'entries',
+  response: AxiosResponse<Bundle | Patient | OperationOutcome>
+): Result => {
+  const transactionStatus = `${response.status} ${response.statusText}`;
+  const result = {
+    'resource id': resourceId,
+    'patient or entries?': resourceName,
+    'transaction status': transactionStatus,
+  };
+
   const statusMap = {};
-  if (response.data && response.data.entry) {
-    response.data.entry.forEach((entry: BundleEntry) => {
+  const processingEntriesUploadSuccesses = response.data && (response.data as Bundle).entry;
+  const processingEntriesOrPatientUploadFailures = response.data && (response.data as OperationOutcome).issue;
+
+  if (processingEntriesUploadSuccesses) {
+    /* Can't expect to see a mix of successes and failures since we're uploading transaction bundles, 
+    which completely fail when even 1 entry fails to upload */
+    const entries = (response.data as Bundle).entry;
+    for (const entry of entries) {
       const entryStatus = entry.response && entry.response.status ? entry.response.status : 'unknown';
       if (!statusMap[entryStatus]) {
         statusMap[entryStatus] = 1;
       } else {
         statusMap[entryStatus] = statusMap[entryStatus] + 1;
       }
-    });
+    }
+  } else if (processingEntriesOrPatientUploadFailures) {
+    const diagnosticMessage = (response.data as OperationOutcome).issue[0].diagnostics;
+    result['diagnostic message'] = diagnosticMessage;
+    statusMap[transactionStatus] = 1;
   } else {
-    statusMap[status] = 1;
+    // Successful patient upload
+    statusMap[transactionStatus] = 1;
   }
 
-  return { 'bundle id': name, 'transaction status': status, 'entry statuses': JSON.stringify(statusMap) };
+  result['resource statuses'] = JSON.stringify(statusMap);
+
+  return result as Result;
 };
 
 const upload = async (release: string, baseURL: string, patientPath: string, dump: boolean): Promise<void> => {
@@ -68,20 +92,17 @@ const upload = async (release: string, baseURL: string, patientPath: string, dum
       continue;
     }
 
-    const json: Bundle = JSON.parse(fs.readFileSync(file, 'utf8'));
+    const json: Resource = JSON.parse(fs.readFileSync(file, 'utf8'));
     if (json.resourceType !== 'Bundle') {
-      console.error('Test patients must be a valid FHIR Bundle.  Not a Bundle:', file);
+      console.error('Test patients must be a valid FHIR Bundle. Not a Bundle:', file);
       continue;
     }
 
-    const request: Promise<Result[]> = getRequest(json, fileName);
+    const request: Promise<Result[]> = getRequest(json as Bundle, fileName);
     requests.push(request);
   }
 
-  const flattenedResults = (await Promise.all(requests)).flat();
-  console.log('url:', baseURL);
-  console.table(flattenedResults);
-  console.log();
+  await printResults(baseURL, requests);
 };
 
 const makeRequester = (
@@ -91,49 +112,73 @@ const makeRequester = (
 ): ((bundle: Bundle, fileName: string) => Promise<Result[]>) => {
   const getRequest = (bundle: Bundle, fileName: string): Promise<Result[]> => {
     const patient: FhirResource = bundle.entry.find(e => e.resource && e.resource.resourceType === 'Patient').resource;
-    const entries: Bundle = JSON.parse(JSON.stringify(bundle));
-    entries.id += '_entries';
-    entries.entry = entries.entry.filter(e => e.resource && e.resource.resourceType !== 'Patient');
+    const entries: Bundle = getEntries(bundle, baseURL);
+    dump && dumpTestPatientJSON(release, fileName, patient, entries);
 
-    const converted: Bundle = cloneAndConvertToTransaction(entries, baseURL);
-    dump && dumpTestPatientJSON(release, fileName, patient, converted);
+    const patientConfig: AxiosRequestConfig = {
+      url: `${baseURL}/Patient/${patient.id}`,
+      method: 'put',
+      headers: { 'Content-Type': 'application/json' },
+      data: JSON.stringify(patient),
+    };
+    const entriesConfig: AxiosRequestConfig = {
+      url: baseURL,
+      method: 'post',
+      headers: { 'Content-Type': 'application/json' },
+      data: JSON.stringify(entries),
+    };
+    const getPatientResult = (id: string, response: AxiosResponse<Patient>) => getResult(id, 'patient', response);
+    const getEntriesResult = (response: AxiosResponse<Bundle>) => getResult(entries.id, 'entries', response);
 
-    const config: AxiosRequestConfig = { headers: { 'Content-Type': 'application/json' } };
+    /* In 2018, there was an issue with the SMART server bugging out when trying to PUT the entire input Bundle. 
+    If we PUT the Patient first and then POST the other entries, that seemed to result in less bugs. */
+    const request: Promise<Result[]> = axios(patientConfig).then(
+      async (success: AxiosResponse<Patient>) => {
+        const patientSuccess = getPatientResult(`${bundle.id}_patient`, success);
 
-    // Due to an issue w/ how the server handles transactions when a patient has been deleted, we post the patient
-    // first and then the other stuff.
-    const request: Promise<Result[]> = axios
-      .put(`${baseURL}/Patient/${patient.id}`, JSON.stringify(patient), config)
-      .then(
-        (success: AxiosResponse<Bundle>) => {
-          const results: Result[] = [];
-          results.push(getResult(`${bundle.id}_patient`, success));
+        const entriesResult: Result = await axios(entriesConfig).then(
+          (success: AxiosResponse<Bundle>) => getEntriesResult(success),
+          (error: AxiosError<Bundle>) => getEntriesResult(error.response)
+        );
 
-          axios.post(baseURL, JSON.stringify(converted), config).then(
-            (success: AxiosResponse<Bundle>) => results.push(getResult(entries.id, success)),
-            (error: AxiosError<Bundle>) => results.push(getResult(entries.id, error.response))
-          );
-
-          return results;
-        },
-        (error: AxiosError<Bundle>) => [getResult(patient.id, error.response)]
-      );
+        return [patientSuccess, entriesResult];
+      },
+      (error: AxiosError<Patient>) => [getPatientResult(patient.id, error.response)]
+    );
 
     return request;
   };
   return getRequest;
 };
 
+const getEntries = (bundle: Bundle, baseURL: string): Bundle => {
+  const clone: Bundle = JSON.parse(JSON.stringify(bundle));
+  clone.id += '_entries';
+  clone.entry = clone.entry.filter(e => e.resource && e.resource.resourceType !== 'Patient');
+
+  const converted: Bundle = convertToTransaction(clone, baseURL);
+  return converted;
+};
+
+const printResults = async (baseURL: string, requests: Promise<Result[]>[]): Promise<void> => {
+  const results = await Promise.all(requests);
+  const flattenedResults = results.flat();
+  console.log('url:', baseURL);
+  console.table(flattenedResults, ['resource id', 'patient or entries?', 'transaction status', 'resource statuses']);
+  console.table(
+    flattenedResults.filter(result => !!result['diagnostic message']),
+    ['resource id', 'diagnostic message']
+  );
+};
+
 const main = (): void => {
   const SMART_HEALTH_IT_ENDPOINT = 'https://r4.smarthealthit.org';
-  const MOONSHOT_ENDPOINT = 'http://moonshot-dev.mitre.org:4006/baseR4';
 
   const args = process.argv.slice(2);
   const dump = args.length !== 0 && args[0] === '--dump';
   const uploadR4 = (baseURL: string) => upload('R4', baseURL, path.join(__dirname, 'r4_test_patients'), dump);
 
   uploadR4(SMART_HEALTH_IT_ENDPOINT);
-  uploadR4(MOONSHOT_ENDPOINT);
 };
 
 main();
