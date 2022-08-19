18273336039ec8db805cda39d14714bd5dcb4427
jtquach1 <jtquach1@gmail.com>
2022-08-19 13:21:56 -0400

    Fix tests for pillbox fields on search page

--- cypress/e2e/search.cy.ts
+++ cypress/e2e/search.cy.ts
@@ -1,18 +1,23 @@
 import * as searchServerSideProps from '../fixtures/searchServerSideProps.json';
 
-const containsEveryExpectedItem = (arr: string[]) => () =>
-  cy.get(`[role="button"]`).each((item, index) => cy.wrap(item).contains(arr[index]));
+const hasEveryExpectedItem = (arr: string[]) => () =>
+  cy
+    .get(`[role="button"]`)
+    .each((item, index) => cy.wrap(item).contains(arr[index]))
+    .should('exist');
+
+const hasNoItems = () => cy.get(`[role="button"]`).should('not.exist');
 
 const hasText = (value: string) => () => cy.get(`input[type="text"]`).should('have.value', value);
 
 const hasNumber = (value: number) => () => cy.get(`input[type="number"]`).should('have.value', value);
 
-const isEmpty = () => cy.get(`input[type="text"]`).should('be.empty');
+const textIsEmpty = () => cy.get(`input[type="text"]`).should('be.empty');
+
+const numberIsEmpty = () => cy.get(`input[type="number"]`).should('be.empty');
 
 beforeEach(() => {
   cy.viewport('macbook-16');
-
-  // Skip the launch page since we are not loading data from the EHR
   cy.visit('/search', { failOnStatusCode: false });
 });
 
@@ -31,13 +36,11 @@ describe('Tests the search page', () => {
   } = searchServerSideProps.props;
 
   const selectedTravelDistance = '1';
-  const selectedAge = '30';
   const selectedZipcode = '02215';
   const selectedCancerType = 'Sarcoma of breast (disorder)';
   const selectedCancerSubtype = 'Juvenile carcinoma of the breast (morphologic abnormality)';
   const selectedStage = 'II';
-  const selectedEcogScore = '4';
-  const selectedKarnofskyScore = '30';
+  const selectedKarnofskyScore = '20';
   const selectedMetastasis = 'metastasis-2';
   const selectedMedication = 'medication-1';
 
@@ -53,65 +56,106 @@ describe('Tests the search page', () => {
     cy.get('[data-testid="zipcode"]').within(hasText(patient.zipcode));
     cy.get('[data-testid="age"]').within(hasNumber(Number(patient.age)));
     cy.get('[data-testid="cancerType"]').within(hasText(primaryCancerCondition.cancerType.display));
-    cy.get('[data-testid="cancerSubtype"]').within(isEmpty);
+    cy.get('[data-testid="cancerSubtype"]').within(textIsEmpty);
     cy.get('[data-testid="stage"]').within(hasText(primaryCancerCondition.stage));
     cy.get('[data-testid="ecogScore"]').within(hasText(ecogScore));
     cy.get('[data-testid="karnofskyScore"]').within(hasText(karnofskyScore));
-    cy.get('[data-testid="metastasis"]').within(containsEveryExpectedItem(metastasis));
-    cy.get('[data-testid="biomarkers"]').within(containsEveryExpectedItem(biomarkers));
-    cy.get('[data-testid="radiation"]').within(containsEveryExpectedItem(radiation));
-    cy.get('[data-testid="surgery"]').within(containsEveryExpectedItem(surgery));
-    cy.get('[data-testid="medications"]').within(containsEveryExpectedItem(medications));
+    cy.get('[data-testid="metastasis"]').within(hasEveryExpectedItem(metastasis));
+    cy.get('[data-testid="biomarkers"]').within(hasEveryExpectedItem(biomarkers));
+    cy.get('[data-testid="radiation"]').within(hasEveryExpectedItem(radiation));
+    cy.get('[data-testid="surgery"]').within(hasEveryExpectedItem(surgery));
+    cy.get('[data-testid="medications"]').within(hasEveryExpectedItem(medications));
   });
 
-  it.only('selects different options for the fields on the search form', () => {
+  it('is not submitted without zipcode', () => {
+    cy.get('input:invalid').should('have.length', 0);
+
+    cy.get('[data-testid="zipcode"]').click().clear();
+    cy.get('[type="submit"]').click();
+    cy.get('input:invalid').should('have.length', 1);
+
+    cy.get('[data-testid="zipcode"]').within(() => {
+      cy.get('[type="text"]').then($input => {
+        expect(($input[0] as HTMLObjectElement).validationMessage).to.eq('Please fill out this field.');
+      });
+    });
+  });
+
+  it('selects different options for the fields on the search form', () => {
     cy.wait(1000);
 
+    // Uncheck default matching service
     cy.get('[data-testid="matchingServices"]')
       .get('[name="matchingServices.service-2"]')
       .click()
-      .should('not.be.checked'); // uncheck default matching service
+      .should('not.be.checked');
 
-    // TODO: clear zipcode and attempt to search, see if "Please fill out this field" appears?
-    cy.get('[data-testid="zipcode"]').click().clear().type(selectedZipcode); // select zipcode by clearing and typing
+    // Select zipcode by clearing and typing
+    cy.get('[data-testid="zipcode"]').click().clear().type(selectedZipcode).within(hasText(selectedZipcode));
 
-    cy.get('[data-testid="travelDistance"').click().clear().type(selectedTravelDistance); // select travel distance by clearing and typing
+    // Select travel distance by clearing and typing
+    cy.get('[data-testid="travelDistance"')
+      .click()
+      .clear()
+      .type(selectedTravelDistance)
+      .within(hasText(selectedTravelDistance));
 
-    cy.get('[data-testid="age"]').type('{upArrow}{upArrow}{esc}'); // fail to select age with arrow keys and hitting escape
+    // Fail to input non-number for age
+    cy.get('[data-testid="age"]').clear().type('abc').within(numberIsEmpty);
 
+    // Select cancer type option by clicking
     cy.get('[data-testid="cancerType"]')
       .click()
       .get('.MuiAutocomplete-popper [role="listbox"]')
       .contains(selectedCancerType)
-      .click(); // clicked cancer type option
+      .click();
+    cy.get('[data-testid="cancerType"]').within(hasText(selectedCancerType));
 
-    cy.get('[data-testid="cancerSubtype"]').type('juvenile{downArrow}{enter}'); // select option by typing and arrow keys and enter
+    // Select option by typing and arrow keys and enter
+    cy.get('[data-testid="cancerSubtype"]').type('juvenile{downArrow}{enter}').within(hasText(selectedCancerSubtype));
 
+    // Clicked stage option
     cy.get('[data-testid="stage"]')
       .click()
       .get('.MuiAutocomplete-popper [role="listbox"]')
       .contains(selectedStage)
-      .click(); // clicked stage option
+      .click();
+    cy.get('[data-testid="stage"]').within(hasText(selectedStage));
 
-    cy.get('[data-testid="ecogScore"]').click().type('INVALID OPTION').should('not.contain', selectedEcogScore); // fail to select valid option and reset to default
+    // Fail to select valid option and reset to default
+    cy.get('[data-testid="ecogScore"]').click().type('INVALID OPTION');
+    cy.contains('No options').should('exist');
 
-    cy.get('[data-testid="karnofskyScore"]').type('{downArrow}{downArrow}{downArrow}{enter}'); // selected option with arrow keys and enter
+    // Selected option with arrow keys and enter
+    cy.get('[data-testid="karnofskyScore"]')
+      .type('{downArrow}{downArrow}{downArrow}{enter}')
+      .within(hasText(selectedKarnofskyScore));
 
+    // Clicked metastasis option
     cy.get('[data-testid="metastasis"]')
       .click()
       .get('.MuiAutocomplete-popper [role="listbox"]')
       .contains(selectedMetastasis)
-      .click(); // clicked metastasis option
+      .click();
+    cy.get('[data-testid="metastasis"]').within(hasEveryExpectedItem([...metastasis, selectedMetastasis]));
 
-    cy.get('[data-testid="biomarkers"]').click().find('[title="Clear"]').click(); // removed option by clicking on field X button
+    // Removed option by clicking on field X button
+    cy.get('[data-testid="biomarkers"]').click().find('[title="Clear"]').click();
+    cy.get('[data-testid="biomarkers"]').within(hasNoItems);
 
-    cy.get('[data-testid="radiation"]').within(() => {
-      cy.get(`[role="button"]`).find('[data-testid="CancelIcon"]').click();
-    }); // removed option by clicking on pill X button
+    // Removed option by clicking on pill X button
+    cy.get('[data-testid="radiation"]').within(() =>
+      cy.get(`[role="button"]`).find('[data-testid="CancelIcon"]').click()
+    );
+    cy.get('[data-testid="radiation"]').within(hasNoItems);
 
-    cy.get('[data-testid="surgery"]').type('{leftArrow}{backspace}{backspace}'); // removed option with arrow keys and backspace
+    // Removed option with arrow keys and backspace
+    cy.get('[data-testid="surgery"]').type('{leftArrow}{backspace}{backspace}');
+    cy.get('[data-testid="surgery"]').within(hasEveryExpectedItem([surgery[0], surgery[1]]));
 
-    cy.get('[data-testid="medications"]').type(`${selectedMedication}{enter}{downArrow}{downArrow}{enter}`); // removed option with arrow keys and enter
+    // Removed option with arrow keys and enter
+    cy.get('[data-testid="medications"]').type(`${selectedMedication}{enter}{downArrow}{downArrow}{enter}`);
+    cy.get('[data-testid="medications"]').within(hasEveryExpectedItem(medications));
   });
 
   it('goes to the results page when the search button is clicked', () => {
