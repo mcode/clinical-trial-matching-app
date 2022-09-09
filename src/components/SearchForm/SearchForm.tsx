8b29e28bbd5e2a4af0c1410ba801c4190c578fd7
Napoleon Russ <nruss@mitre.org>
2022-09-09 15:20:11 -0400

    fix reference to SearchFormOptions

--- src/components/SearchForm/SearchForm.tsx
+++ src/components/SearchForm/SearchForm.tsx
@@ -6,7 +6,7 @@ import Image from 'next/image';
 import { useRouter } from 'next/router';
 import { ReactElement, useState } from 'react';
 import { Controller, useForm } from 'react-hook-form';
-import { cancerTypeDetails } from 'src/components/SearchForm/SearchFormOptions';
+import { cancerTypeDetails } from 'src/components/SearchForm/SearchFormOptions.tsx';
 import { SearchParameters } from 'types/search-types';
 import {
   AgeTextField,
