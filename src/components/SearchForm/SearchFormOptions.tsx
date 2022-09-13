f8e1da6d373510ebbbd1fd6bf6d7c50ff8b71d5c
Dan Potter <dpotter@mitre.org>
2022-09-13 11:28:43 -0400

    Fix import paths

--- src/components/SearchForm/SearchFormOptions.tsx
+++ src/components/SearchForm/SearchFormOptions.tsx
@@ -1,29 +1,29 @@
 import { CodedValueType } from '@/utils/fhirConversionUtils';
-import BRAIN_BIOMARKER_CODES from 'src/queries/mockData/brainBiomarkerCodes.json';
-import BRAIN_CANCER_TYPE_CODES from 'src/queries/mockData/brainCancerTypeCodes.json';
-import BRAIN_MEDICATION_CODES from 'src/queries/mockData/brainMedicationCodes.json';
-import BREAST_CANCER_BIOMARKERS from 'src/queries/mockData/breastCancerBiomarkerCodes.json';
-import BREAST_CANCER_MEDICATIONS from 'src/queries/mockData/breastCancerMedicationCodes.json';
-import BREAST_CANCER_STAGES from 'src/queries/mockData/breastCancerStageCodes.json';
-//import BreastCancerSurgeryCodes from 'src/queries/mockData/breastCancerSurgeryCodes.json';
-import BREAST_CANCER_TYPE_CODES from 'src/queries/mockData/breastCancerTypeCodes.json';
-import COLON_CANCER_TYPE_CODES from 'src/queries/mockData/colonCancerTypeCodes.json';
-import COLON_MEDICATION_CODES from 'src/queries/mockData/colonMedicationCodes.json';
-import COLON_RADIATION_CODES from 'src/queries/mockData/colonRadiationCodes.json';
-import COLON_SURGERY_CODES from 'src/queries/mockData/colonSurgeryCodes.json';
+import BRAIN_BIOMARKER_CODES from '../../queries/mockData/brainBiomarkerCodes.json';
+import BRAIN_CANCER_TYPE_CODES from '../../queries/mockData/brainCancerTypeCodes.json';
+import BRAIN_MEDICATION_CODES from '../../queries/mockData/brainMedicationCodes.json';
+import BREAST_CANCER_BIOMARKERS from '../../queries/mockData/breastCancerBiomarkerCodes.json';
+import BREAST_CANCER_MEDICATIONS from '../../queries/mockData/breastCancerMedicationCodes.json';
+import BREAST_CANCER_STAGES from '../../queries/mockData/breastCancerStageCodes.json';
+//import BreastCancerSurgeryCodes from '../../queries/mockData/breastCancerSurgeryCodes.json';
+import BREAST_CANCER_TYPE_CODES from '../../queries/mockData/breastCancerTypeCodes.json';
+import COLON_CANCER_TYPE_CODES from '../../queries/mockData/colonCancerTypeCodes.json';
+import COLON_MEDICATION_CODES from '../../queries/mockData/colonMedicationCodes.json';
+import COLON_RADIATION_CODES from '../../queries/mockData/colonRadiationCodes.json';
+import COLON_SURGERY_CODES from '../../queries/mockData/colonSurgeryCodes.json';
 import {
   default as BRAIN_CANCER_SUBTYPE_CODES,
   default as BRAIN_RADIATION_CODES,
   default as BRAIN_SURGERY_CODES,
-} from 'src/queries/mockData/empty.json';
-import LUNG_CANCER_SUBTYPE_CODES from 'src/queries/mockData/lungCancerSubTypeCodes.json';
-import LUNG_CANCER_TYPE_CODES from 'src/queries/mockData/lungCancerTypeCodes.json';
-import LUNG_MEDICATION_CODES from 'src/queries/mockData/lungMedicationCodes.json';
-import LUNG_RADIATION_CODES from 'src/queries/mockData/lungRadiationCodes.json';
-import LUNG_SURGERY_CODES from 'src/queries/mockData/lungSurgeryCodes.json';
-import MULTIPLE_MYELOMA_CANCERTYPE_CODES from 'src/queries/mockData/MultipleMyelomaCancerTypeCodes.json';
-import MULTIPLE_MYELOMA_MEDICATION_CODES from 'src/queries/mockData/MultipleMyelomaMedicationCodes.json';
-import PROSTATE_CANCER_TYPE_CODES from 'src/queries/mockData/prostateCancerTypeCodes.json';
+} from '../../queries/mockData/empty.json';
+import LUNG_CANCER_SUBTYPE_CODES from '../../queries/mockData/lungCancerSubTypeCodes.json';
+import LUNG_CANCER_TYPE_CODES from '../../queries/mockData/lungCancerTypeCodes.json';
+import LUNG_MEDICATION_CODES from '../../queries/mockData/lungMedicationCodes.json';
+import LUNG_RADIATION_CODES from '../../queries/mockData/lungRadiationCodes.json';
+import LUNG_SURGERY_CODES from '../../queries/mockData/lungSurgeryCodes.json';
+import MULTIPLE_MYELOMA_CANCERTYPE_CODES from '../../queries/mockData/MultipleMyelomaCancerTypeCodes.json';
+import MULTIPLE_MYELOMA_MEDICATION_CODES from '../../queries/mockData/MultipleMyelomaMedicationCodes.json';
+import PROSTATE_CANCER_TYPE_CODES from '../../queries/mockData/prostateCancerTypeCodes.json';
 
 export type CancerTypeDetails = {
   category: string;
