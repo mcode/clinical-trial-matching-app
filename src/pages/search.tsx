b561620b078bc9e49cf0fc2d585ca17e734c1bfd
Dan Potter <dpotter@mitre.org>
2023-04-20 09:44:58 -0400

    Check response to make sure it had data

--- src/pages/search.tsx
+++ src/pages/search.tsx
@@ -224,18 +224,19 @@ const getAllEncounters = (fhirClient: Client) => {
 
 // This assumes that all of the encounters are in order by date.
 const getMostRecentPerformanceValue = async (fhirClient: Client, encounters: fhirclient.FHIR.Bundle, code: string) => {
-  console.log('encounters length ', encounters.entry.length);
   for (let i = 0; i < encounters.entry.length; i++) {
     const encounter = encounters.entry[i];
     const observations = await fhirClient.request<fhirclient.FHIR.Bundle>(
       `Observation?patient=${fhirClient.getPatientId()}&category=smartdata&focus=${encounter.resource.id}`
     );
-    console.log(`Encounter SDES ${encounter.resource.id} ${observations.entry.length}`);
-    const found = observations.entry.find(
-      entry => entry.resource.resourceType === 'Observation' && entry.resource.code?.coding.some(c => c.code === code)
-    );
-    if (found) {
-      return found.resource;
+    if (Array.isArray(observations.entry)) {
+      console.log(`Encounter SDES ${encounter.resource.id} ${observations.entry.length}`);
+      const found = observations.entry.find(
+        entry => entry.resource.resourceType === 'Observation' && entry.resource.code?.coding.some(c => c.code === code)
+      );
+      if (found) {
+        return found.resource;
+      }
     }
   }
   return undefined;
