fa24fa6d82cf15ec85b2f7c782247f84ee554e56
Daniel Potter <dpotter@mitre.org>
2023-05-12 15:38:58 -0400

    Refactor matching service config

--- next.config.js
+++ next.config.js
@@ -1,3 +1,35 @@
+// List of active services. These are used to pass the service configuration within SERVICES below
+// within the publicRuntimeConfig.
+const ACTIVE_SERVICES = ['ancora', 'breastCancerTrials', 'carebox', 'lungevity', 'trialjectory'];
+
+// If true, uses http://localhost/${id}/getClinicalTrial as the service URL, otherwise,
+// uses http://localhost:${service.defaultPort}/getClinicalTrial.
+const SERVICES_ON_SAME_SERVER = false;
+
+const SERVICES = {
+  ancora: {
+    label: 'Ancora.ai',
+    defaultPort: 3002,
+  },
+  breastCancerTrials: {
+    label: 'BreastCancerTrials.org',
+    defaultPort: 3000,
+    defaultValue: true,
+  },
+  carebox: {
+    label: 'Carebox',
+    defaultPort: 3004,
+  },
+  lungevity: {
+    label: 'LUNGevity',
+    defaultPort: 3003,
+  },
+  trialjectory: {
+    label: 'TrialJectory',
+    defaultPort: 3001,
+  },
+};
+
 module.exports = {
   watch: true, // Enable watch mode
   poweredByHeader: false,
@@ -9,33 +41,19 @@ module.exports = {
     defaultTravelDistance: process.env.DEFAULT_TRAVEL_DISTANCE,
     sendLocationData: JSON.parse(process.env.SEND_LOCATION_DATA ?? 'false'),
     reactAppDebug: JSON.parse(process.env.REACT_APP_DEBUG ?? 'false'),
-    services: [
-      {
-        name: 'ancora',
-        label: 'Ancora.ai',
-        url: 'http://localhost',
-        searchRoute: '/ancora.ai/getClinicalTrial',
-      },
-      {
-        name: 'breastCancerTrials',
-        label: 'BreastCancerTrials.org',
-        url: 'http://localhost',
-        searchRoute: '/breastcancertrials.org/getClinicalTrial',
-        defaultValue: true,
-      },
-      {
-        name: 'lungevity',
-        label: 'LUNGevity',
-        url: 'http://localhost',
-        searchRoute: '/lungevity/getClinicalTrial',
-      },
-      {
-        name: 'carebox',
-        label: 'Carebox',
-        url: 'http://localhost',
-        searchRoute: '/carebox/getClinicalTrial',
-      },
-    ],
+    services: ACTIVE_SERVICES.map(id => {
+      const service = SERVICES[id];
+      if (!service) {
+        throw Error(`Unknown active service "${id}".`);
+      }
+      return {
+        name: id,
+        label: service.label,
+        url: SERVICES_ON_SAME_SERVER ? 'http://localhost' : `http://localhost:${service.defaultPort}`,
+        searchRoute: SERVICES_ON_SAME_SERVER ? `/${id}/getClinicalTrial` : '/getClinicalTrial',
+        defaultValue: service.defaultValue ?? false,
+      };
+    }),
   },
   serverRuntimeConfig: {
     sessionSecretKey: process.env.SESSION_SECRET_KEY,
