7c7f4ca7ac211391a5f286537891cd593e37d7bf
Daniel Potter <dpotter@mitre.org>
2023-07-25 15:12:14 -0400

    Upgrades to Next.js 12
    The primary reason for this is to resolve an issue with MD4 usage in
    the version of Webpack used by Next.js 11.

--- next.config.js
+++ next.config.js
@@ -24,7 +24,6 @@ const matchingServices = enabledMatchingServices.split(/\s*,\s*/).map(service =>
 });
 
 module.exports = {
-  watch: true, // Enable watch mode
   poweredByHeader: false,
   reactStrictMode: true,
   publicRuntimeConfig: {
