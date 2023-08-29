f9d98ca3a165b0645cfd9750bcaac3a5f9dae294
Dan Potter <dpotter@mitre.org>
2023-08-29 12:31:03 -0400

    Attempt to function under IE11

--- src/pages/search.tsx
+++ src/pages/search.tsx
@@ -1,3 +1,5 @@
+import 'react-app-polyfill/ie11';
+import 'react-app-polyfill/stable';
 import Header from '@/components/Header';
 import PatientCard from '@/components/PatientCard';
 import SearchForm from '@/components/SearchForm';
