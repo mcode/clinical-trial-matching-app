bc2e7c7c4f2bb16cbfc454223895625c389e4bba
Christine Duong <cqduong@mitre.org>
2023-10-08 17:26:56 -0400

    Restrict on cancer type

--- next.config.js
+++ next.config.js
@@ -1,3 +1,4 @@
+const allowedCancerTypes = ['bladder', 'brain', 'breast', 'colon', 'lung', 'multipleMyeloma', 'prostate'];
 const enabledMatchingServices = process.env.MATCHING_SERVICES;
 if (!enabledMatchingServices || /^\s*$/.test(enabledMatchingServices)) {
   console.error(
@@ -9,12 +10,17 @@ const defaultMatchingServices = new Set((process.env.MATCHING_SERVICES_DEFAULT_E
 
 const matchingServices = enabledMatchingServices.split(/\s*,\s*/).map(service => {
   const serviceEnvName = service.toUpperCase();
+  const cancerTypes = process.env[`MATCHING_SERVICE_${serviceEnvName}_CANCER_TYPES`]
+    .split(/\s*,\s*/)
+    .filter(cancer => allowedCancerTypes.includes(cancer));
+
   return {
     name: service,
     label: process.env[`MATCHING_SERVICE_${serviceEnvName}_LABEL`] ?? service,
     url: process.env[`MATCHING_SERVICE_${serviceEnvName}_URL`] ?? `http://localhost/${service}`,
     searchRoute: '/getClinicalTrial',
     defaultValue: defaultMatchingServices.has(service),
+    cancerTypes: cancerTypes,
   };
 });
 
