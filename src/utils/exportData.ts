f5a3c797cb1a127545c5d1fd4946e5c19098cdad
Daniel Potter <dpotter@mitre.org>
2023-11-29 14:21:23 -0500

    Remove commented out code

--- src/utils/exportData.ts
+++ src/utils/exportData.ts
@@ -1,10 +1,8 @@
 // NOTE: Uncomment this out for facilities information
 //import { getContact } from '@/components/Results/utils';
 import { stringify as csvStringify } from 'csv-stringify/sync';
-//import FileSaver from 'file-saver';
 import { FullSearchParameters } from 'types/search-types';
 import { v4 as uuidv4 } from 'uuid';
-//import XLSX from 'xlsx';
 import { StudyDetail, StudyDetailProps } from '../components/Results/types';
 
 // NOTE: Uncomment this out for facilities information
@@ -121,16 +119,6 @@ const convertToSpreadsheetRow = (details: StudyDetail[], userId: string): Record
   }
   return newDatum;
 };
-
-/*export const exportSpreadsheetData = (data: Record<string, string>[], fileName: string): void => {
-  const fileType = 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet;charset=UTF-8';
-  const fileExtension = '.xlsx';
-  const worksheet = XLSX.utils.json_to_sheet(data);
-  const workbook = { Sheets: { data: worksheet }, SheetNames: ['data'] };
-  const excelBuffer = XLSX.write(workbook, { bookType: 'xlsx', type: 'array' });
-  const blob = new Blob([excelBuffer], { type: fileType });
-  FileSaver.saveAs(blob, fileName + fileExtension);
-};*/
 
 export const exportCsvStringData = (patientSearch: FullSearchParameters, data: StudyDetailProps[]): string => {
   const patientElements = convertPatientInfoToRedCapRow(patientSearch);
