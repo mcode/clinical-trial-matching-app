67fc6ce542c2506ba8452b80bb817bfe4b76d17c
Daniel Potter <dpotter@mitre.org>
2024-04-11 16:00:06 -0400

    Refactor patient data fetch
    - Remove different Epic/sandbox methods
    - Load multiple pages rather than only one
    - Better use of TypeScript generics

--- src/utils/fhir/fetch.ts
+++ src/utils/fhir/fetch.ts
@@ -1,9 +1,19 @@
-import type { Bundle, BundleEntry, FhirResource } from 'fhir/r4';
+import { Bundle, BundleEntry, FhirResource, Medication, MedicationRequest } from 'fhir/r4';
 import type Client from 'fhirclient/lib/Client';
 
-export async function fetchBundle<T extends FhirResource>(fhirClient: Client, resourceType: T['resourceType']): Promise<Bundle> {
-  return await fhirClient.request<Bundle>(`${resourceType}?patient=${fhirClient.getPatientId()}`);
-}
+/**
+ * Attempts to request all pages of a given resource type. Each page will be
+ * returned as a separate Bundle.
+ * @param fhirClient the FHIR client
+ * @param resourceType the resource type to fetch
+ * @returns an array of bundles
+ */
+export const fetchBundles = async <T extends FhirResource>(
+  fhirClient: Client,
+  resourceType: T['resourceType']
+): Promise<Bundle[]> => {
+  return await fhirClient.request<Bundle[]>(`${resourceType}?patient=${fhirClient.getPatientId()}`, { pageLimit: 0 });
+};
 
 /**
  * This method fetches a given bundle containing resources of the given type and then returns only the bundle entries
@@ -12,15 +22,62 @@ export async function fetchBundle<T extends FhirResource>(fhirClient: Client, re
  * @param resourceType
  * @returns
  */
-export async function fetchBundleEntries<T extends FhirResource>(fhirClient: Client, resourceType: T['resourceType']): Promise<BundleEntry<T>[]> {
-  const bundle = await fetchBundle(fhirClient, resourceType);
-  // If the bundle exists, return a filtered copy of just the entries array
-  if (bundle && bundle.entry) {
-    return bundle.entry.filter<BundleEntry<T>>((entry): entry is BundleEntry<T> => entry.resource?.resourceType === resourceType);
+export const fetchBundleEntries = async <T extends FhirResource>(
+  fhirClient: Client,
+  resourceType: T['resourceType']
+): Promise<BundleEntry<T>[]> => {
+  const result: BundleEntry<T>[] = [];
+  const bundles = await fetchBundles(fhirClient, resourceType);
+  for (const bundle of bundles) {
+    // If the bundle exists, return a filtered copy of just the entries array
+    if (bundle && bundle.entry) {
+      result.push(
+        ...bundle.entry.filter<BundleEntry<T>>(
+          (entry): entry is BundleEntry<T> => entry.resource?.resourceType === resourceType
+        )
+      );
+    }
   }
-  // Default: return an empty array
-  return [];
-}
+  return result;
+};
+
+/**
+ * Fetches all resources of the given type from the FHIR client. This discards the bundle entry objects returned from
+ * fetchBundleEntries<T>().
+ * @param fhirClient the FHIR client
+ * @param resourceType the resource type to fetch
+ * @returns all resources of the given type
+ */
+export const fetchResources = async <T extends FhirResource>(
+  fhirClient: Client,
+  resourceType: T['resourceType']
+): Promise<T[]> => (await fetchBundleEntries(fhirClient, resourceType)).map<T>(entry => entry.resource);
+
+/**
+ * Retrieves all known medications from the current patient.
+ * @param fhirClient the client to retrieve medications from
+ */
+export const fetchMedications = async (fhirClient: Client): Promise<Medication[]> => {
+  const medications: Promise<Medication>[] = [];
+  for (const medRequest of await fetchResources<MedicationRequest>(fhirClient, 'MedicationRequest')) {
+    console.log('Checking medication request');
+    console.log(JSON.stringify(medRequest, null, 2));
+    // See if this requires the medication be loaded separately
+    if (medRequest.medicationReference?.reference) {
+      // It does, so add the request
+      medications.push(fhirClient.request<Medication>(medRequest.medicationReference.reference));
+    } else if (medRequest.medicationCodeableConcept) {
+      // Has the medication embedded
+      medications.push(
+        Promise.resolve({
+          resourceType: 'Medication',
+          code: medRequest.medicationCodeableConcept,
+        })
+      );
+    }
+  }
+  return Promise.all(medications);
+};
 
 /**
  * Determines if a given resource has a profile. This can be used in the Array filter method to filter out resources
@@ -31,4 +88,4 @@ export async function fetchBundleEntries<T extends FhirResource>(fhirClient: Cli
  */
 export const resourceHasProfile = (resource: FhirResource, profile: string): boolean => {
   return (resource.meta?.profile?.indexOf(profile) ?? -1) >= 0;
-}
+};
