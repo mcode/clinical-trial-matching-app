bac4cabbd5f9faf00b79e47925679fac6820fd20
Christine Duong <cqduong@mitre.org>
2024-09-18 17:48:56 -0400

    Set zipcode and travel distance explicitly if disabled

--- src/components/SearchForm/SearchForm.tsx
+++ src/components/SearchForm/SearchForm.tsx
@@ -108,6 +108,13 @@ const SearchForm = ({ defaultValues, fullWidth, setUserId, disableLocation }: Se
   const userId = useContext(UserIdContext);
 
   const onSubmit = (data: SearchFormValuesType) => {
+    if (disableLocation) {
+      // If location data was disabled, it won't be in the form data, and we'd
+      // want to ignore anything in the fields anyway, so overwrite it
+      data.zipcode = defaultValues.zipcode;
+      data.travelDistance = defaultValues.travelDistance;
+    }
+    
     const query = {
       ...formDataToSearchQuery(data),
       sortingOption: 'matchLikelihood',
