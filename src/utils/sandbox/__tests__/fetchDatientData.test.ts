a09d34b679fb22624040e0ce37532fb7fc37df06
Daniel Potter <dpotter@mitre.org>
2024-09-19 14:45:13 -0400

    Remove eom fields from tests

--- src/utils/sandbox/__tests__/fetchDatientData.test.ts
+++ src/utils/sandbox/__tests__/fetchDatientData.test.ts
@@ -58,48 +58,4 @@ describe('buildPatientData', () => {
     expect(typeof patientData).toEqual('object');
     expect(patientData).not.toBeNull();
   });
-  it('finds disease status records', () => {
-    const patientData = buildPatientData([
-      testPatient,
-      testUser,
-      [],
-      [
-        // This is an abridged version of the example cancer disease status from the mCODE IG
-        {
-          resourceType: 'Observation',
-          id: 'cancer-disease-status-improved-brian-l',
-          meta: {
-            profile: ['http://hl7.org/fhir/us/mcode/StructureDefinition/mcode-cancer-disease-status'],
-          },
-          status: 'final',
-          code: {
-            coding: [
-              {
-                system: 'http://loinc.org',
-                code: '97509-4',
-              },
-            ],
-          },
-          subject: {
-            reference: 'Patient/cancer-patient-brian-l',
-          },
-          effectiveDateTime: '2024-02-08',
-          valueCodeableConcept: {
-            coding: [
-              {
-                system: 'http://snomed.info/sct',
-                code: '268910001',
-                display: "Patient's condition improved (finding)",
-              },
-            ],
-          },
-        },
-      ],
-      [],
-      [],
-    ]);
-    expect(patientData.diseaseStatus).not.toBeNull();
-    expect(patientData.diseaseStatus.system).toEqual('http://snomed.info/sct');
-    expect(patientData.diseaseStatus.code).toEqual('268910001');
-  });
 });
